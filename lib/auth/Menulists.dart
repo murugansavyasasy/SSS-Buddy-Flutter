4933856a320d31b0123116712df47e09b1d1d96f
murugansavyasasy <murugan@savyasasy.com>
2026-05-08 16:35:20 +0530

    toursettlement hide

--- lib/auth/Menulists.dart
+++ lib/auth/Menulists.dart
@@ -8,7 +8,7 @@ final List<MenuItem> menuItems = [
   MenuItem(id: 4,  title: "Circular List",         icon: Icons.announcement,          color: Colors.purple),
   MenuItem(id: 5,  title: "Status Report",         icon: Icons.assignment_turned_in,  color: Colors.purpleAccent),
   MenuItem(id: 6,  title: "Record Collection",     icon: Icons.payment,               color: Colors.pink),
-  MenuItem(id: 7,  title: "Tour Settlement",       icon: Icons.receipt_long,          color: Colors.teal),
+  // MenuItem(id: 7,  title: "Tour Settlement",       icon: Icons.receipt_long,          color: Colors.teal),
   MenuItem(id: 8,  title: "Advance Tour Expense",  icon: Icons.attach_money,          color: Colors.deepOrange),
   MenuItem(id: 9,  title: "Local Conveyence",      icon: Icons.directions_car,        color: Colors.indigo),
   MenuItem(id: 10, title: "Customer Details",      icon: Icons.business,              color: Colors.cyan),
