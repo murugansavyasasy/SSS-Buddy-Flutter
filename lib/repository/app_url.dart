a375da093b7d56085f5791cc10aba934872fd72c
murugansavyasasy <murugan@savyasasy.com>
2026-08-25 17:45:07 +0530

    by murugan

--- lib/repository/app_url.dart
+++ lib/repository/app_url.dart
@@ -1,6 +1,5 @@
 class AppUrl {
   static String vimsUrl = "http://score.savyasasy.com/";
-  static String schoolUrl =
-      "https://api.schoolchimes.com/nodejs/api/SalesDemo/";
+  static String schoolUrl = "https://api.schoolchimes.com/nodejs/api/SalesDemo/";
   static String aws_url = "https://api.schoolchimes.com/nodejs/api/MergedApi/";
 }
